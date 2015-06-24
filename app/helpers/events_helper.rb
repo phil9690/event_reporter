8510ae9e1d779301e124c77205a29fe0e2f3f9f9
Phil <p.clauzel90@gmail.com>
2015-06-24 15:09:50 +0100

    Check if employee has suspension before creating

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,19 +1,29 @@
 module EventsHelper
 
-	def logged_by(user)
-		"#{user.first_name} #{user.last_name}".titleize
-	end
+  def logged_by(user)
+    "#{user.first_name} #{user.last_name}".titleize
+  end
 
-	def is_unread(unreads, event)  
-		unreads.each do |unread|
-			if event.id == unread.event_id
-				return "unread"
-			end
-		end
-	end
-	     
-#		params[:action] == action && params[:controller] == controller ? "active" : nil        
-#	end
-	
+  def is_unread(unreads, event)  
+    unreads.each do |unread|
+      if event.id == unread.event_id
+        return "unread"
+      end
+    end
+  end
+
+  def check_for_suspension(event, employee)
+    if event.incident_type == "Suspension"
+      if Suspension.active.find_by(employee_id: event.employee_id).present?
+        flash.now[:danger] = "This employee is currently suspended"
+        render 'new'
+      else
+        Suspension.create(employee_id: event.employee_id, event_id: event.id)
+        redirect_to employee_event_path(employee, event)
+      end
+    else
+      redirect_to employee_event_path(employee, event)
+    end
+  end
 
 end
