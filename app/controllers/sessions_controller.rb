31104e3482db277f7ac7ede1afc6261716f1eda4
Phil <p.clauzel90@gmail.com>
2015-09-01 14:15:49 +0100

    If logged in redirect to user path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,27 @@
 class SessionsController < ApplicationController
 
-	#before_action :logged_in_user, only: [:new, :create]
+  #before_action :logged_in_user, only: [:new, :create]
 
   def new
+    if logged_in?
+      redirect_to current_user
+    end 
   end
-  
+
   def create
-  	user = User.find_by(username: params[:session][:username].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		log_in user
-  		redirect_to user
-  	else
-  		flash.now[:danger] = 'Invalid email/password combination' #not right
-  		render 'new'
-  	end
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' #not right
+      render 'new'
+    end
   end
-  
+
   def destroy
-  	log_out
-  	redirect_to root_url
+    log_out
+    redirect_to root_url
   end
-  
+
 end
