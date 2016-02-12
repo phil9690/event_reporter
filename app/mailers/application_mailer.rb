cb80532f3c5847ebde20a531a3287e2bee864b75
Phil <p.clauzel90@gmail.com>
2016-02-12 12:21:31 +0000

    Add emails to users

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,18 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "EventReports@populusdatasolutions.com"
+
+  default from: "event-reporter@populusdatasolutions.com"
   layout 'mailer'
+
+  def all_emails
+    emails = []
+
+    User.all.each do |user|
+      if user.email.present?
+        emails.push(user.email)
+      end    
+    end 
+
+    emails
+  end
+
 end
--- app/mailers/report.rb
+++ app/mailers/report.rb
@@ -3,6 +3,7 @@ class Report < ApplicationMailer
   def daily_report
     @events = Event.where("created_at >= ?", Time.zone.now.beginning_of_day)
     @today = Date.today.strftime('%d/%m/%Y')
-    mail(to: "supervisors@populusdatasolutions.com", subject: "Events for #{@today}")
+    mail(to: all_emails, subject: "Events for #{@today}")
   end
+
 end
