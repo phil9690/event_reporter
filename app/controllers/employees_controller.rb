cd5bd0ff9e28dd92c4789fc9c49ef8aba608717d
Phil <p.clauzel90@gmail.com>
2016-02-12 13:48:42 +0000

    Add trainee boolean to employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -58,6 +58,6 @@ class EmployeesController < ApplicationController
 
   private
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :active, :pid)
+    params.require(:employee).permit(:first_name, :last_name, :active, :pid, :trainee)
   end
 end
